7a5168a63ab76d9ca4f0940834c74abd804f97be
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-07-12 16:31:52 -0500

    Examen 2 Calderón_Flores

--- app/src/main/java/com/labs_danp/examen2_danp/FbMessageService.kt
+++ app/src/main/java/com/labs_danp/examen2_danp/FbMessageService.kt
@@ -1,34 +1,57 @@
 package com.labs_danp.examen2_danp
 
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+
 class FbMessageService : FirebaseMessagingService() {
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        showNotification(message.data["message"])
+    private val db = Firebase.firestore
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        showNotification(
+            remoteMessage.notification!!.title, remoteMessage.notification!!.body
+        )
     }
 
-    private fun showNotification(message: String?) {
+    private fun showNotification(title: String?, message: String?) {
         val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-
-        val pendingIntent =
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-        val builder = NotificationCompat.Builder(this)
-            .setAutoCancel(true)
-            .setContentTitle("FCM Test")
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(title)
             .setContentText(message)
-            .setSmallIcon(com.google.android.gms.base.R.drawable.common_google_signin_btn_icon_dark)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT) // Set the intent that will fire when the user taps the notification
             .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        val notificationManager = NotificationManagerCompat.from(this)
 
-        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        manager.notify(0, builder.build())
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, builder.build())
+    }
 
+    override fun onNewToken(token: String) {
+        val newToken = hashMapOf("token" to token)
+        db.collection("tokens_devices")
+            .add(newToken)
+            .addOnSuccessListener { documentReference ->
+                Log.d("NewTokenSuccess", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+            Log.w("NewTokenFailure", "Error adding document", e)
+        }
+    }
+
+    companion object {
+        private const val TAG = "PushNotification"
+        private const val CHANNEL_ID = "101"
     }
 }
--- app/src/main/java/com/labs_danp/examen2_danp/MainActivity.kt
+++ app/src/main/java/com/labs_danp/examen2_danp/MainActivity.kt
@@ -1,37 +1,86 @@
 package com.labs_danp.examen2_danp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 
+
 class MainActivity : AppCompatActivity() {
+
+    private val db = Firebase.firestore
+    private val TAG = "PushNotification"
+    private val CHANNEL_ID = "101"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        notification()
+        createNotificationChannel()
+        getToken()
 
     }
 
-    private fun notification() {
+    private fun getToken() {
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
-                Log.w("TOKEN", "Fetching FCM registration token failed", task.exception)
+                Log.d(TAG, "Fetching FCM registration token failed", task.exception)
                 return@OnCompleteListener
             }
 
             // Get new FCM registration token
             val token = task.result
+            Log.d(TAG, "Completed: $token")
 
-            // Log and toast
-            val msg = token
-            Log.e("TOKENNNNN", token)
-            Toast.makeText(baseContext, "Abierto Satisfactoriamente", Toast.LENGTH_SHORT).show()
+            var flag = true
+            db.collection("tokens_devices")
+                .get()
+                .addOnSuccessListener { result ->
+                    val idx = result.size()
+                    var index = 0
+                    for (document in result) {
+                        if((document.data["token"].toString() == token)){
+                            flag = false
+                            Log.e("CAMBIO", "La bandera es $flag")
+                        }
+                        index += 1
+                        if (index == idx && flag){
+                            val newToken = hashMapOf("token" to token)
+                            db.collection("tokens_devices")
+                                .add(newToken)
+                                .addOnSuccessListener { documentReference ->
+                                    Log.d(
+                                        "NewTokenSuccess",
+                                        "DocumentSnapshot added with ID: ${documentReference.id}"
+                                    )
+                                }
+                                .addOnFailureListener { e ->
+                                    Log.w("NewTokenFailure", "Error adding document", e)
+                                }
+                        }
+                    }
+                }
         })
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = "firebaseNotificationChannel"
+            val description = "Receive Firebase Notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
